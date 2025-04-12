12c6807217dc746484eddd8ef5fed203e9430a62
patilarun05 <patilarun05@gmail.com>
2025-04-13 03:04:45 +0530

    CodeRefactor

---
+++ src/main/java/com/example/Code/Welcome.java
@@ -0,0 +1,9 @@
+package com.example.Code;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+    @GetMapping("/Welcome")
+    public String getData() {return  "Running on Master.." ; }
+}
---
+++ src/test/java/com/example/Code/CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
