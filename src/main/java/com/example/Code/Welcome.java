ba4a73db1ef9787c3a9b57ced81fe282f40477b7
patilarun05 <patilarun05@gmail.com>
2025-04-28 23:51:50 +0530

    Test

--- src/main/java/com/example/Code/Welcome.java
+++ src/main/java/com/example/Code/Welcome.java
@@ -1,9 +1,13 @@
 package com.example.Code;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Welcome {
-    @GetMapping("/Welcome")
-    public String getData() {return  "Welcome Indu to Daddy's world ❤" ; }
+
+    @GetMapping("/welcome")
+    public String getWelcomeMessage() {
+        return "❤❤❤❤❤ Welcome Indu to Daddy's world ❤❤❤❤❤";
+    }
 }
